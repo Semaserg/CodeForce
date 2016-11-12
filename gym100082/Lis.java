c5425d741cec3378e548dfc2b9a818c2bc93f44d
Sergey <Semaserg@gmail.com>
2016-11-11 21:25:41 -0800

    first commit

---
+++ gym100082/Lis.java
@@ -0,0 +1,12 @@
+package CodeForce.gym100082;
+
+import java.util.Scanner;
+
+public class Lis {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Printing the file passed in:");
+        while(sc.hasNextLine()) System.out.println(sc.nextLine());
+        System.out.print("End");
+    }
+}
